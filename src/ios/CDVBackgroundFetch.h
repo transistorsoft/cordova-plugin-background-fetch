37bd1e176486145f6d484da89c66b65632d0cb4f
Chris Scott <christocracy@gmail.com>
2013-10-17 13:16:55 -0400

    Fleshing out ios plugin

--- src/ios/CDVBackgroundFetch.h
+++ src/ios/CDVBackgroundFetch.h
@@ -1,1 +1,21 @@
-CDVBackgroundFetch.h
+//
+//  CDVBackgroundGeoLocation.hs
+//
+//  Created by Chris Scott <chris@transistorsoft.com>
+//
+
+#import <Cordova/CDVPlugin.h>
+
+@interface CDVBackgroundFetch : CDVPlugin <UIApplicationDelegate>
+- (void) configure:(CDVInvokedUrlCommand*)command;
+- (void) start:(CDVInvokedUrlCommand*)command;
+- (void) stop:(CDVInvokedUrlCommand*)command;
+- (void) test:(CDVInvokedUrlCommand*)command;
+- (void) sync;
+- (void) onSuspend:(NSNotification *)notification;
+- (void) onResume:(NSNotification *)notification;
+
+@property(nonatomic,assign) BOOL enabled;
+
+@end
+
--- src/ios/CDVBackgroundFetch.m
+++ src/ios/CDVBackgroundFetch.m
@@ -1,1 +1,52 @@
-CDVBackgroundFetch.m
+////
+//  CDVBackgroundGeoLocation
+//
+//  Created by Chris Scott <chris@transistorsoft.com> on 2013-06-15
+//  Largely based upon http://www.mindsizzlers.com/2011/07/ios-background-location/
+//
+#import "CDVLocation.h"
+#import "CDVBackgroundFetch.h"
+#import <Cordova/CDVJSON.h>
+
+@implementation CDVBackgroundFetch
+
+@synthesize enabled;
+
+
+- (void) configure:(CDVInvokedUrlCommand*)command
+{
+    
+    NSLog(@"CDVBackgroundFetch configure");
+}
+
+- (void) start:(CDVInvokedUrlCommand*)command
+{
+    NSLog(@"CDVBackgroundFetch start");
+    self.enabled = YES;
+}
+
+- (void) stop:(CDVInvokedUrlCommand*)command
+{
+    NSLog(@"CDVBackgroundFetch stop");
+    self.enabled = NO;
+}
+
+- (void) test:(CDVInvokedUrlCommand*)command
+{
+    NSLog(@"CDVBackgroundFetch test");
+}
+
+// If you don't stopMonitorying when application terminates, the app will be awoken still when a
+// new location arrives, essentially monitoring the user's location even when they've killed the app.
+// Might be desirable in certain apps.
+- (void)applicationWillTerminate:(UIApplication *)application 
+{
+    
+}
+
+- (void)dealloc
+{
+    
+}
+
+@end
